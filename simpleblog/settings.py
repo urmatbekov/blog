8801d461cc3a68689376a30e473d872e252a582f
Eldiyar <eldiiardev@gmail.com>
2020-08-31 10:08:09 +0600

    cors all

--- simpleblog/settings.py
+++ simpleblog/settings.py
@@ -89,11 +89,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-CORS_ORIGIN_WHITELIST = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
