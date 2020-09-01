b87faa98c7135d44fe4e3a2d6ed6e013450b5377
Eldiyar <eldiiardev@gmail.com>
2020-09-01 15:30:13 +0600

    image blog

--- simpleblog/serializers.py
+++ simpleblog/serializers.py
@@ -18,4 +18,4 @@ class NewSerializerEdit(serializers.ModelSerializer):
 class NewSerializerDetail(serializers.ModelSerializer):
     class Meta:
         model = New
-        fields = ['id', 'title', 'type', 'body', 'author', 'image_blog', 'created_at', 'updated_at']
+        fields = ['id', 'title', 'type', 'body', 'author', 'image_detail', 'created_at', 'updated_at']
