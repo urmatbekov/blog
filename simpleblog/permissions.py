58a196fa0f951f2198d2a1f0d667e48bd53e63e6
Eldiyar <eldiiardev@gmail.com>
2020-09-15 17:10:56 +0600

    is_staff

--- simpleblog/permissions.py
+++ simpleblog/permissions.py
@@ -12,6 +12,8 @@ class IsOwnerOrReadOnly(BasePermission):
         # so we'll always allow GET, HEAD or OPTIONS requests.
         if request.method in SAFE_METHODS:
             return True
+        if request.user.id and request.user.is_staff:
+            return True
 
         # Instance must have an attribute named `owner`.
         return obj.owner == request.user
